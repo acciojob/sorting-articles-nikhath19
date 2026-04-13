7a3e45583c665299435dac01b0bdef61f49e7308
nikhath19 <nikhathsultana8367@gmail.com>
2026-04-13 21:13:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ const sortedBands = bands.sort((a, b)
 							  => strip(a) > strip(b) ? 1 : -1);
 
 }
-document.querySelecto
-r('#band').innerHTML
+document.querySelector('#band').innerHTML
 =
 	sortedBands.map(band => '<li>${band}
 		</li>').join('');
