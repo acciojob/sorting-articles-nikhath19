2851c530157ef99894edbe995cfc69cdbdd87d09
nikhath19 <nikhathsultana8367@gmail.com>
2026-05-13 23:43:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-//your JS code here. If required.
-const bands = ['The Plot in You',
-			  'The Devil Wears Prada', 'Pierce the
-				   Veil', 'Norma Jean', 'The Bled', 'Say
-	Anything', 'The Midway State', 'We Came as Romans', 'Counterparts',
-		'Oh, 'Sleeper', 'A Skylit Drive', 'Anywhere
-		But Here', 'An Old Dog'];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
+// Article ignore karne ka function
 function strip(bandName) {
-	return bandName.replace(/^(a |an |
-							the )/i, '').trim();
+  return bandName.replace(/^(a |an |the )/i, '').trim();
 }
-const sortedBands = bands.sort((a, b)
- => strip(a) > strip(b) ? 1 : -1);
 
-}
-document.querySelector('#band').innerHTML =
-	sortedBands.map(band => `<li>${band}</li>`).join('');
+// Sorting logic
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
+// HTML mein display karna
+document.querySelector('#bands').innerHTML = 
+  sortedBands.map(band => <li>${band}</li>).join('');
